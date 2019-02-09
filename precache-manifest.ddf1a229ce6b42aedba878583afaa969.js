316b9604c9e0a237d411279b60b7eb055b3b1ade
Anabel <anabelpazb@gmail.com>
2019-02-09 01:24:08 -0300

    Updates

---
+++ precache-manifest.ddf1a229ce6b42aedba878583afaa969.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "77f5aee24aaf337194dd",
+    "url": "/Juego-Ahorcado/static/js/runtime~main.77f5aee2.js"
+  },
+  {
+    "revision": "676c2d87d89aa689f436",
+    "url": "/Juego-Ahorcado/static/js/main.676c2d87.chunk.js"
+  },
+  {
+    "revision": "410c6f281a233f7c9ad9",
+    "url": "/Juego-Ahorcado/static/js/1.410c6f28.chunk.js"
+  },
+  {
+    "revision": "676c2d87d89aa689f436",
+    "url": "/Juego-Ahorcado/static/css/main.7e4fe9fc.chunk.css"
+  },
+  {
+    "revision": "410c6f281a233f7c9ad9",
+    "url": "/Juego-Ahorcado/static/css/1.47bcc745.chunk.css"
+  },
+  {
+    "revision": "023cc84de9ce56df3e21308ee4703378",
+    "url": "/Juego-Ahorcado/index.html"
+  }
+];
