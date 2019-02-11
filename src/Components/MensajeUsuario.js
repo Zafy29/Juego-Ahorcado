876477d2d2019e06d9d995ad474d32a0e3da0465
Anabel <anabelpazb@gmail.com>
2019-02-11 20:34:37 -0300

    Corrección de ortografía

--- src/Components/MensajeUsuario.js
+++ src/Components/MensajeUsuario.js
@@ -2,32 +2,26 @@ import React from "react";
 
 const MensajeUsuario = props => {
   // //mensaje para indicar al usuario
-  switch(props.msg){
+  switch (props.msg) {
     //al fallar intento
     case "error":
-    return (
-    <div className="alert alert-danger">
-        <strong>¡Letra incorrecta!</strong> intentos restantes: {props.intentosFallidos}
-      </div>);
+      return (
+        <div className="alert alert-danger">
+          <b>¡Letra incorrecta!</b>
+          Intentos restantes: {props.intentosFallidos}
+        </div>
+      );
     //completar palabra
     case "completado":
-      return (
-      <div className="alert alert-success">
-      Completado!</div>
-      );
+      return <div className="alert alert-success">¡Completado!</div>;
     //letra ingresada repetida
     case "repite":
       return (
-    <div className="alert alert-warning">¡Ya esa letra fue ingresada!</div>
-    );
+        <div className="alert alert-warning">¡Ya esa letra fue ingresada!</div>
+      );
     //por defecto.
     default:
-      return (
-      <div className="alert alert-info">
-      Ingrese una letra</div>
-      );
-
+      return <div className="alert alert-info">Ingrese una letra.</div>;
   }
- 
 };
 export default MensajeUsuario;
